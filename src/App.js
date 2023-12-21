4c047c5a762bc5f9ba87d282d947764296107014
kwisdomkk <kwisdom@naver.com>
2023-12-21 13:05:15 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "리액트";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <span className="title">카카오톡</span>
+        <span className="menu">카톡 안녕가이드 카톡설명서 다운로드</span>
+      </div>
+      <hr />
+      <div className="main">
+        <h1>이모티콘</h1>
+        <div>
+          우리들의 새로운 언어, 이모티콘! <br />
+          글자만으로는 내 감정을 표현하기 어려운 순간들
+          <br />
+          고민하지 말고 이모티콘을 이용해보세요!
+        </div>
+      </div>
+    </>
   );
 }
 
